2bbd6a11676777d763dcb69075b8e3b746e3df36
Naveen <Naveen@LAPTOP-BQHQGI82>
2023-02-06 01:33:42 +0530

    Add project files.

---
+++ DBConnectionTest/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace DBConnectionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.severNameBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputTextBox2 = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.userNametextBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Name *";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Databases *";
+            // 
+            // severNameBox1
+            // 
+            this.severNameBox1.Location = new System.Drawing.Point(192, 77);
+            this.severNameBox1.Name = "severNameBox1";
+            this.severNameBox1.Size = new System.Drawing.Size(422, 22);
+            this.severNameBox1.TabIndex = 2;
+            this.severNameBox1.TextChanged += new System.EventHandler(this.severNameBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(192, 153);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output folder *";
+            // 
+            // outputTextBox2
+            // 
+            this.outputTextBox2.Location = new System.Drawing.Point(192, 194);
+            this.outputTextBox2.Name = "outputTextBox2";
+            this.outputTextBox2.ReadOnly = true;
+            this.outputTextBox2.Size = new System.Drawing.Size(422, 22);
+            this.outputTextBox2.TabIndex = 0;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(630, 188);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 34);
+            this.browseButton.TabIndex = 5;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.maskedTextBox1);
+            this.panel1.Controls.Add(this.userNametextBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.browseButton);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.outputTextBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 6;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(500, 116);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(205, 22);
+            this.maskedTextBox1.TabIndex = 9;
+            this.maskedTextBox1.TextChanged += new System.EventHandler(this.maskedTextBox1_TextChanged);
+            // 
+            // userNametextBox1
+            // 
+            this.userNametextBox1.Location = new System.Drawing.Point(192, 116);
+            this.userNametextBox1.Name = "userNametextBox1";
+            this.userNametextBox1.Size = new System.Drawing.Size(206, 22);
+            this.userNametextBox1.TabIndex = 8;
+            this.userNametextBox1.TextChanged += new System.EventHandler(this.userNametextBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(425, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Username";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Windows Authentication",
+            "SQL Server Authentication"});
+            this.comboBox2.Location = new System.Drawing.Point(112, 34);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(325, 24);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(630, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Test Connection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.severNameBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DbTestWinApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox severNameBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox outputTextBox2;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox userNametextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DBConnectionTest/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBConnectionTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string DataSource = "";
+        public static string OutputFolder = "";
+        public static string Username = "";
+        public static string Password = "";
+        public static string AuthType = "";
+        public static string connectionString ="";
+
+
+        public void getValues() 
+        {
+             DataSource = severNameBox1.Text.Trim();
+             OutputFolder = outputTextBox2.Text.Trim();
+            Username = userNametextBox1.Text.Trim();
+            Password = maskedTextBox1.Text.Trim();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            outputTextBox2.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        public bool validatedata()
+        {
+           
+            if (string.IsNullOrEmpty(severNameBox1.Text) || string.IsNullOrEmpty(comboBox1.Text) || string.IsNullOrEmpty(outputTextBox2.Text))
+            {
+                MessageBox.Show("Please fill all mandetory fields");
+                return false;
+            }
+            else
+            {
+               
+                return true;
+            }
+        
+        }
+        
+        public bool validateserver()
+        {
+            if (AuthType == "Windows Authentication")
+            {
+                if (string.IsNullOrEmpty(severNameBox1.Text))
+                {
+                    MessageBox.Show("Please fill server name");
+                    return false;
+                }
+                else
+                {
+
+                    return true;
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(severNameBox1.Text))
+                {
+                    MessageBox.Show("Please fill server name");
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(userNametextBox1.Text) || string.IsNullOrEmpty(maskedTextBox1.Text))
+                {
+                    MessageBox.Show("Please enter username and password");
+                    return false;
+                }
+                else
+                {
+
+                    return true;
+                }
+            }
+           
+
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (validatedata())
+            {
+                MessageBox.Show("Test completed");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            getValues();
+            if (validateserver())
+            {
+                if (AuthType == "Windows Authentication")
+                {
+                    connectionString = $"Data Source={DataSource};Integrated Security=True";        
+                }
+                else
+                {
+                    connectionString = $"Data Source={DataSource}; user id={Username};password={Password};";
+                }
+                //string connectionString = $"Data Source={DataSource};Integrated Security=True";            // LAPTOP-BQHQGI82
+                SqlConnection conn = new SqlConnection(connectionString);
+                try
+                {
+
+                    conn.Open();
+                    MessageBox.Show("Sever Connection established");
+
+                    var cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "Select name from sys.databases";
+
+                    var adapter = new SqlDataAdapter(cmd);
+                    var dataset = new DataSet();
+                    adapter.Fill(dataset);
+                    DataTable dtDatabases = dataset.Tables[0];
+                    //comboBox1.Items.Add("----Choose database----");
+                    for (int i = 0; i < dtDatabases.Rows.Count; i++)
+                    {
+
+                        comboBox1.Items.Add(dtDatabases.Rows[i][0].ToString());
+                        conn.Close();
+                    }
+                    MessageBox.Show("Databases loaded.");
+                    //comboBox1.SelectedText = "test1";
+                    comboBox1.SelectedIndex = 0;
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void severNameBox1_TextChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+        }
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            AuthType = comboBox2.SelectedItem.ToString();
+            if (!(AuthType== "Windows Authentication"))
+            {
+                userNametextBox1.Visible = true;
+                maskedTextBox1.Visible = true;
+                label4.Visible = true;
+                label5.Visible = true;
+            }
+            else
+            {
+                userNametextBox1.Visible = false;
+                maskedTextBox1.Visible = false;
+                label4.Visible = false;
+                label5.Visible = false;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.SelectedIndex = 0;
+            AuthType = comboBox2.SelectedItem.ToString();
+            userNametextBox1.Visible = false;
+            maskedTextBox1.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+        }
+
+      
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+        }
+
+        private void userNametextBox1_TextChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+        }
+    }
+}
